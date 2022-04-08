c525842fa38135e12c22ff9db2bcca5fd82f4ba5
Mthrun <m.thrun@gmx.net>
2022-04-08 07:44:24 +0200

    bugfix

--- R/fitParameters.R
+++ R/fitParameters.R
@@ -14,9 +14,15 @@ fitParameters=function(Feature,ClassInd,Robust=FALSE,na.rm=TRUE,Threshold=0.0001
   # Parameters[1:2]    Numeric vector with Mean and Std.
   # 
   Feature_Class=Feature[ClassInd]
+   if(!requireNamespace("dbt.Statistics")){
+   Robust=FALSE
+   warning("fitParameters: requires internal package dbt.Statistic to work which is not installed, setting Robust=FALSE")
+   }
   if(isTRUE(Robust)){
+ 
     me=dbt.Statistics::meanrobust(Feature_Class)
     std=dbt.Statistics::stdrobust(Feature_Class)
+
   }else{
     me=mean(Feature_Class,na.rm=na.rm)
     std=sd(Feature_Class,na.rm=na.rm)
